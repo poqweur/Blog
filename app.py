2bb5214cf2e3f0af38c63a63079fc65f747ae0b8
练习用户1 <1149447019@qq.com>
2018-01-02 21:43:10 +0800

    显示页面

---
+++ app.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+import logging;logging.basicConfig(level=logging.DEBUG)
+#设置日志
+import asyncio,os,json,time
+from datetime import datetime
+import aiomysql
+
+
+from aiohttp import web
+
+def index(request):
+    #需要设置content type类型是html,否则会下载
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',8000)
+    logging.info('server started at http://127.0.0.1:8000...')
+    return srv
+
+
+
+loop=asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
